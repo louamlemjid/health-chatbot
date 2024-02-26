d4cdcebecd7f1c052777168b7762a0402cb2faa3
louamlemjid <elmejidlouem@gmail.com>
2024-02-26 13:06:25 +0100

    tag route

--- app.js
+++ app.js
@@ -23,34 +23,77 @@ app.use(express.static("public"));
 
 
 app.get('/', (req, res) => {
-    res.render("home", { generatedText: req.session.generatedText });
+    try{
+        if(req.session.tag){
+            res.render("home", { generatedText: req.session.generatedText,tag:req.session.tag,load:req.session.loading });
+        }else{
+            res.render("home", { generatedText: req.session.generatedText });
+        }
+        
+    }catch(error){
+        console.error("error : ",error)
+        res.send('error : ',error)
+    }
 });
-
-
+app.post("/tag",(req,res)=>{
+    try{
+        req.session.tag=req.body.tag;
+        res.redirect('/')
+    }catch(error){
+        console.error("error",error);
+        res.send('error',error)
+    }
+})
 app.post("/text", async (req, res) => {
     try {
-        const options = {
-            method: "POST",
-            url: "https://api.edenai.run/v2/text/chat",
-            headers: {
-                authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZjgyYmMzMDItNDA5OC00MDgyLWI4NmQtODI3ZDgwNjMzZWY0IiwidHlwZSI6ImFwaV90b2tlbiJ9.h0fV0VOjjaP4jFPHpCSyVCCubXUgHdYwKdiMBORKSs8",
-            },
-            data: {
-                providers: "openai",
-                text: req.body.text,
-                chatbot_global_action: "health assist",
-                previous_history: [],
-                temperature: 1, // Adjust temperature to control randomness of generated text
-                fallback_providers: "",
-            },
-        };
-
-        const response = await axios.request(options);
-        console.log(response.data.openai.generated_text);
+        req.session.loading=true;
+        if(req.session.tag){
+            const options = {
+                method: "POST",
+                url: "https://api.edenai.run/v2/text/chat",
+                headers: {
+                    authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZjgyYmMzMDItNDA5OC00MDgyLWI4NmQtODI3ZDgwNjMzZWY0IiwidHlwZSI6ImFwaV90b2tlbiJ9.h0fV0VOjjaP4jFPHpCSyVCCubXUgHdYwKdiMBORKSs8",
+                },
+                data: {
+                    providers: "openai",
+                    text: req.body.text,
+                    chatbot_global_action:req.session.tag,
+                    previous_history: [],
+                    temperature: 1,
+                    fallback_providers: "",
+                },
+            };
+            const response = await axios.request(options);
+            console.log(response.data.openai.generated_text);
 
         req.session.generatedText = response.data.openai.generated_text;
+        }else{
+            const options = {
+                method: "POST",
+                url: "https://api.edenai.run/v2/text/chat",
+                headers: {
+                    authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZjgyYmMzMDItNDA5OC00MDgyLWI4NmQtODI3ZDgwNjMzZWY0IiwidHlwZSI6ImFwaV90b2tlbiJ9.h0fV0VOjjaP4jFPHpCSyVCCubXUgHdYwKdiMBORKSs8",
+                },
+                data: {
+                    providers: "openai",
+                    text: req.body.text,
+                    chatbot_global_action:"health assistant",
+                    previous_history: [],
+                    temperature: 1,
+                    fallback_providers: "",
+                },
+            };
+            const response = await axios.request(options);
+            console.log(response.data.openai.generated_text);
+
+            req.session.generatedText = response.data.openai.generated_text;
+
+        }
+
+        
     } catch (error) {
         console.error(error);
+        res.send("error",error)
     }
 
     res.redirect('/');
